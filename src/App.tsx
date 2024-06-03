2855ee15e10bbdfaefde6d1d9abfd46e88eed2df
thanaporn.ch <thanaporn_chan@cmu.ac.th>
2024-06-03 22:45:59 +0700

    #add: cmuOAuth

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,43 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { Radio, Group } from "@mantine/core";
-import "@mantine/core/styles.css";
-import "tailwindcss/tailwind.css";
 import "./App.css";
-import Login from "./pages/login";
+import { useDispatch, useSelector } from "react-redux";
+import { setUser } from "@store/user";
+import Login from "@pages/login";
+import Page404 from "@pages/Page404";
+import { getUserInfo } from "@services/user/user.service";
+import CMUOAuthCallback from "@pages/cmuOAuthCallback";
+import { removeLocalStorage } from "@helpers/functions/localStorage";
+import { ROUTE_PATH } from "@helpers/constants/route";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const user = useSelector((state: any) => state.user.value);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (user.role) return;
+
+    const fetchData = async () => {
+      const res = await getUserInfo();
+      if (res.role) {
+        dispatch(setUser(res));
+      } else {
+        removeLocalStorage("token");
+      }
+    };
+
+    fetchData();
+  }, [user, dispatch]);
 
   return (
-    
     <Router>
       <Routes>
-        <Route exact path="/" element={<Login />} />
+        <Route path={ROUTE_PATH.LOGIN} element={<Login />} />
+        <Route
+          path={ROUTE_PATH.CMU_OAUTH_CALLBACK}
+          element={<CMUOAuthCallback />}
+        />
+        <Route path="*" element={<Page404 />} />
       </Routes>
     </Router>
   );
--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,7 +1,6 @@
-import React from "react";
-import cmulogo from "../assets/image/cmuLogo.png";
-import entlogo from "../assets/image/entLogo.png";
-import cmulogoLogin from "../assets/image/cmuLogoLogin.png";
+import cmulogo from "@assets/image/cmuLogo.png";
+import entlogo from "@assets/image/entLogo.png";
+import cmulogoLogin from "@assets/image/cmuLogoLogin.png";
 import { Button } from "@mantine/core";
 
 export default function Login() {
@@ -27,7 +26,7 @@ export default function Login() {
         <a href={import.meta.env.VITE_NEXT_PUBLIC_CMU_OAUTH_URL}>
           <Button
             style={{ borderRadius: "12px" }}
-            className="bg-white mt-1  text-[#696AA9] font-sf-pro-rounded font-medium text-[14px] border-0 h-10 hover:bg-[#DFDFDF] hover:text-[#696AA9] hover:border-0 focus:outline-0"
+            className="bg-white mt-1 text-[#696AA9] font-sf-pro-rounded font-medium text-[14px] h-10 hover:bg-[#DFDFDF] hover:text-[#696AA9]"
           >
             <img
               src={cmulogoLogin}
