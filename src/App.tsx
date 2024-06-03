058b8bd2f80291b304d306be773edb1ec8e64034
thanaporn.ch <thanaporn_chan@cmu.ac.th>
2024-06-04 01:14:54 +0700

    #add: store

--- src/App.tsx
+++ src/App.tsx
@@ -2,33 +2,38 @@ import React, { useEffect, useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import { useDispatch, useSelector } from "react-redux";
-import { setUser } from "@store/user";
-import Login from "@pages/login";
-import Page404 from "@pages/Page404";
-import { getUserInfo } from "@services/user/user.service";
-import CMUOAuthCallback from "@pages/cmuOAuthCallback";
-import { removeLocalStorage } from "@helpers/functions/localStorage";
-import { ROUTE_PATH } from "@helpers/constants/route";
-import SelectDepartment from "@pages/selectDepartment";
+import { setUser } from "@/store/user";
+import Login from "@/pages/login";
+import Page404 from "@/pages/Page404";
+import { getUserInfo } from "@/services/user/user.service";
+import CMUOAuthCallback from "@/pages/cmuOAuthCallback";
+import { removeLocalStorage } from "@/helpers/functions/localStorage";
+import { ROUTE_PATH } from "@/helpers/constants/route";
+import SelectDepartment from "@/pages/selectDepartment";
+import { IModelUser } from "./models/ModelUser";
 
 function App() {
-  const user = useSelector((state: any) => state.user.value);
+  const user: IModelUser = useSelector((state: any) => state.user.value);
   const dispatch = useDispatch();
+  const path = window.location.pathname;
 
   useEffect(() => {
-    if (user.role) return;
+    if (user.role || path == ROUTE_PATH.CMU_OAUTH_CALLBACK) return;
 
     const fetchData = async () => {
       const res = await getUserInfo();
       if (res.role) {
         dispatch(setUser(res));
       } else {
-        removeLocalStorage("token");
+        if (path != ROUTE_PATH.LOGIN) {
+          removeLocalStorage("token");
+          window.location.replace(ROUTE_PATH.LOGIN);
+        }
       }
     };
 
     fetchData();
-  }, [user, dispatch]);
+  }, []);
 
   return (
     <Router>
@@ -38,7 +43,10 @@ function App() {
           path={ROUTE_PATH.CMU_OAUTH_CALLBACK}
           element={<CMUOAuthCallback />}
         />
-        <Route path={ROUTE_PATH.SELECTED_DEPARTMENT} element={<SelectDepartment />} />
+        <Route
+          path={ROUTE_PATH.SELECTED_DEPARTMENT}
+          element={<SelectDepartment />}
+        />
         <Route path="*" element={<Page404 />} />
       </Routes>
     </Router>
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,17 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import { MantineProvider } from "@mantine/core";
+import "@/styles/style.css";
 import "./index.css";
+import "@mantine/core/styles.css";
+import "tailwindcss/tailwind.css";
+import { Provider } from "react-redux";
+import store from "@/store/index.ts";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <MantineProvider>
-    <App />
-  </MantineProvider>
+  <Provider store={store}>
+    <MantineProvider>
+      <App />
+    </MantineProvider>
+  </Provider>
 );
