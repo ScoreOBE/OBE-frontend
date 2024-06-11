db3678c0c882080d7fd0cbee10c93f9dd968dbd1
thanaporn.ch <thanaporn_chan@cmu.ac.th>
2024-06-11 20:11:24 +0700

    #feature: search course

--- src/services/course/dto/course.dto.ts
+++ src/services/course/dto/course.dto.ts
@@ -3,6 +3,7 @@ export class CourseRequestDTO {
   manage: boolean = false;
   orderBy: string = "courseNo";
   orderType: string = "asc";
+  search: string = "";
   page: number = 1;
   limit: number = 20;
 }
