55029b24d2f5fc413748d4098fd3cb27dbbbfd8c
auswitch <90751246+auswitch@users.noreply.github.com>
2024-06-16 01:07:02 +0700

    edit add course modal step3

--- src/components/Modal/ModalAddCourse.tsx
+++ src/components/Modal/ModalAddCourse.tsx
@@ -1,11 +1,14 @@
 import { useState } from "react";
-import { Stepper, Button, Group, Modal, Select } from "@mantine/core";
 import {
-  IconChevronDown,
-  IconCircleFilled,
-  IconMailOpened,
-  IconShieldCheck,
-} from "@tabler/icons-react";
+  Stepper,
+  Button,
+  Group,
+  Modal,
+  Select,
+  TextInput,
+  Textarea,
+} from "@mantine/core";
+import { IconChevronDown, IconCircleFilled } from "@tabler/icons-react";
 import { COURSE_TYPE } from "@/helpers/constants/enum";
 type Props = {
   opened: boolean;
@@ -37,8 +40,8 @@ export default function ModalAddCourse({ opened, onClose }: Props) {
       centered
       transitionProps={{ transition: "pop" }}
       classNames={{
-        title: "text-primary font-semibold text-[18px] f",
-        content: "justify-center item-center px-4",
+        title: "text-primary font-semibold text-[18px]",
+        content: "justify-center item-center px-2",
       }}
     >
       <Stepper
@@ -46,16 +49,22 @@ export default function ModalAddCourse({ opened, onClose }: Props) {
         color="#6869AD"
         onStepClick={setActive}
         allowNextStepsSelect={false}
+        icon={<IconCircleFilled size={300} />}
         classNames={{
           separator: "text-primary -mx-2 mb-12 h-[3px] -translate-x-6",
-          step: "flex flex-col  items-start",
-          stepIcon: "mb-2 text-primary",
+          step: "flex flex-col  items-start mr-2",
+          stepIcon: "mb-2 text-[#E6E6FF] bg-[#E6E6FF]  border-[#E6E6FF]",
           stepBody: "flex-col-reverse m-0",
         }}
-        className=" justify-center items-center "
+        className=" justify-center items-center mt-3"
       >
-        <Stepper.Step label="Course Type" description="STEP 1">
+        <Stepper.Step
+          label="Course Type"
+          classNames={{ stepLabel: "text-[14px]" }}
+          description="STEP 1"
+        >
           <Select
+            clearable
             label="Type of Course"
             placeholder="Select Type of Course"
             data={[COURSE_TYPE.GENERAL, COURSE_TYPE.SEL_TOPIC]}
@@ -63,15 +72,14 @@ export default function ModalAddCourse({ opened, onClose }: Props) {
             // onChange={(_value, option) => setSelectedTerm(option)}
             allowDeselect={false}
             withCheckIcon={false}
-            className="rounded-md my-5 border-none "
+            className="rounded-md my-5 border-none  "
             style={{ boxShadow: "0px 1px 4px 0px rgba(0, 0, 0, 0.05)" }}
             classNames={{
-              label: "font-medium mb-1 text-[16px] text-[#3E3E3E]",
-              input: "text-primary font-medium border-primary",
+              label: "font-medium mb-1 text-[14px] text-[#3E3E3E]",
+              input: "text-primary font-medium focus:border-primary",
               option: "hover:bg-[#DDDDF6] text-primary font-medium",
-              dropdown: ""
+              dropdown: "drop-shadow-[0_0px_4px_rgba(0,0,0,0.30)]",
             }}
-            
             rightSection={
               <IconChevronDown
                 className={`${
@@ -83,16 +91,74 @@ export default function ModalAddCourse({ opened, onClose }: Props) {
             onDropdownClose={() => setOpenedDropdown(false)}
           />
         </Stepper.Step>
-        <Stepper.Step label="Course Name" description="STEP 2">
+        <Stepper.Step
+          label="Course Name"
+          classNames={{ stepLabel: "text-[14px]" }}
+          description="STEP 2"
+        >
+          <div className=" flex flex-col gap-4">
+            <TextInput
+              className=" mt-3"
+              classNames={{ input: "focus:border-primary " }}
+              label="Course no."
+              placeholder="Ex. 001102"
+            />
+            <TextInput
+              label="Course name"
+              classNames={{ input: "focus:border-primary " }}
+              placeholder="English 2"
+              value={""}
+            />
+          </div>
+        </Stepper.Step>
+        <Stepper.Step
+          label="Add Section"
+          classNames={{ stepLabel: "text-[14px]" }}
+          description="STEP 3"
+        >
+          <div
+            style={{
+              boxShadow: "0px 0px 4px 0px rgba(0, 0, 0, 0.25)",
+            }}
+            className="w-full mt-2 te h-full p-4 rounded-md gap-4 flex flex-col"
+          >
+            <div className="flex flex-col gap-1">
+              <span className="text-[#3E3E3E] font-medium text-[14px]">
+                Course No.
+              </span>
+              <span className="text-primary font-medium text-[14px]">
+                261361
+              </span>
+            </div>
+            <div className="flex flex-col gap-1">
+              <span className="text-[#3E3E3E] font-medium text-[14px]">
+                Course Name
+              </span>
+              <span className="text-primary font-medium text-[14px]">
+                Software Engineering
+              </span>
+            </div>
+          </div>
+          <div className="text-[#3E3E3E] mt-4 mb-1 font-medium text-[14px]">
+            Section
+          </div>
+          <Textarea
+            classNames={{ input: "focus:border-primary " }}
+            placeholder="Fill Section Number Ex. 001 or 1 (Press Enter for fill the next section)"
+          ></Textarea>
+        </Stepper.Step>
+        <Stepper.Step
+          label="Co-Instructor"
+          classNames={{ stepLabel: "text-[14px]" }}
+          description="STEP 4"
+        >
           <div></div>
         </Stepper.Step>
-        <Stepper.Step label="Add Section" description="STEP 3">
-          <div></div>
-        </Stepper.Step>
-        <Stepper.Step label="Co-Instructor" description="STEP 4">
-          <div></div>
-        </Stepper.Step>
-        <Stepper.Step label="Review" description="STEP 5">
+        <Stepper.Step
+          label="Review"
+          classNames={{ stepLabel: "text-[14px]" }}
+          description="STEP 5"
+        >
           <div></div>
         </Stepper.Step>
       </Stepper>
@@ -100,14 +166,19 @@ export default function ModalAddCourse({ opened, onClose }: Props) {
       <Group className="flex w-full justify-between mt-8">
         <div>
           {active > 0 && (
-            <Button className="rounded-[10px] hover:bg-[#ebebeb] items-center justify-center h-[36px]  border-0" variant="default" justify="start" onClick={prevStep}>
+            <Button
+              className="rounded-[10px] mt-4 hover:bg-[#ebebeb] items-center justify-center h-[36px]  border-0"
+              variant="default"
+              justify="start"
+              onClick={prevStep}
+            >
               Back
             </Button>
           )}
         </div>
         <Button
-        color="#6869AD"
-          className="rounded-[10px] h-[36px] w-[100px]"
+          color="#6869AD"
+          className="rounded-[10px] h-[36px] mt-4 w-[100px]"
           onClick={nextStep}
         >
           {active == 4 ? "Done" : "Next"}
