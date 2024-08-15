a24d76f363261dbd69722ea673ca6769ea0f985d
thanaporn.ch <thanaporn_chan@cmu.ac.th>
2024-08-15 11:52:24 +0700

    #fixed: setTerm in Dashboard

--- src/pages/Course.tsx
+++ src/pages/Course.tsx
@@ -160,83 +160,77 @@ export default function Course() {
             <div className="overflow-y-auto w-full h-fit max-h-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4 p-1">
               {course?.sections.map((sec) => {
                 return (
-                  <>
-                    <div
-                      key={sec.id}
-                      className="card relative justify-between xl:h-[135px] md:h-[120px] cursor-pointer rounded-[4px] hover:bg-[#F3F3F3]"
-                    >
-                      <div className="p-2.5 flex flex-col">
-                        <p className="font-semibold text-sm">
-                          Section {getSectionNo(sec.sectionNo)}
-                        </p>
-                        <div
-                          onClick={(e) => {
-                            e.stopPropagation();
-                          }}
-                        >
-                          {course.addFirstTime && activeTerm ? (
-                            <Menu
-                              trigger="click"
-                              position="bottom-end"
-                              offset={2}
+                  <div
+                    key={sec.id}
+                    className="card relative justify-between xl:h-[135px] md:h-[120px] cursor-pointer rounded-[4px] hover:bg-[#F3F3F3]"
+                  >
+                    <div className="p-2.5 flex flex-col">
+                      <p className="font-semibold text-sm">
+                        Section {getSectionNo(sec.sectionNo)}
+                      </p>
+                      <div
+                        onClick={(e) => {
+                          e.stopPropagation();
+                        }}
+                      >
+                        {course.addFirstTime && activeTerm ? (
+                          <Menu
+                            trigger="click"
+                            position="bottom-end"
+                            offset={2}
+                          >
+                            <Menu.Target>
+                              <IconDots className="absolute top-2 right-2 rounded-full hover:bg-gray-300" />
+                            </Menu.Target>
+                            <Menu.Dropdown
+                              className="rounded-md backdrop-blur-xl bg-white/70 "
+                              style={{
+                                boxShadow:
+                                  "0px 0px 4px 0px rgba(0, 0, 0, 0.25)",
+                              }}
                             >
-                              <Menu.Target>
-                                <IconDots className="absolute top-2 right-2 rounded-full hover:bg-gray-300" />
-                              </Menu.Target>
-                              <Menu.Dropdown
-                                className="rounded-md backdrop-blur-xl bg-white/70 "
-                                style={{
-                                  boxShadow:
-                                    "0px 0px 4px 0px rgba(0, 0, 0, 0.25)",
-                                }}
-                              >
-                                <Menu.Item className="text-[#3E3E3E] font-semibold text-[12px] h-7 w-[180px] ">
-                                  <div
-                                    onClick={openedModalEditSec}
-                                    className="flex items-center gap-2"
-                                  >
-                                    <IconPencilMinus
-                                      stroke={1.5}
-                                      className="h-4 w-4"
-                                    />
-                                    <span>Edit Section</span>
-                                  </div>
-                                </Menu.Item>
-                                <Menu.Item
-                                  className="text-[#FF4747] hover:bg-[#d55757]/10 font-semibold text-[12px] h-7 w-[180px]"
-                                  onClick={openedMainPopupDelCourse}
+                              <Menu.Item className="text-[#3E3E3E] font-semibold text-[12px] h-7 w-[180px] ">
+                                <div
+                                  onClick={openedModalEditSec}
+                                  className="flex items-center gap-2"
                                 >
-                                  <div className="flex items-center gap-2">
-                                    <IconTrash
-                                      className="h-4 w-4"
-                                      stroke={1.5}
-                                    />
-                                    <span>Delete Section</span>
-                                  </div>
-                                </Menu.Item>
-                              </Menu.Dropdown>
-                            </Menu>
-                          ) : (
-                            <Switch
-                              size="xs"
-                              className="absolute top-3 right-3"
-                              defaultChecked
-                              // checked={sec.isActive}
-                            />
-                          )}
-                        </div>
-                      </div>
-                      <div className="bg-[#e7eaff] flex h-8 items-center justify-between rounded-b-[4px]">
-                        <p className="p-2.5 text-secondary font-semibold text-[12px]">
-                          {(sec.assignments?.length ?? 0) === 1
-                            ? "Assignment"
-                            : (sec.assignments?.length ?? 0) > 1
-                            ? "Assignments"
-                            : "No Assignment"}
-                        </p>
+                                  <IconPencilMinus
+                                    stroke={1.5}
+                                    className="h-4 w-4"
+                                  />
+                                  <span>Edit Section</span>
+                                </div>
+                              </Menu.Item>
+                              <Menu.Item
+                                className="text-[#FF4747] hover:bg-[#d55757]/10 font-semibold text-[12px] h-7 w-[180px]"
+                                onClick={openedMainPopupDelCourse}
+                              >
+                                <div className="flex items-center gap-2">
+                                  <IconTrash className="h-4 w-4" stroke={1.5} />
+                                  <span>Delete Section</span>
+                                </div>
+                              </Menu.Item>
+                            </Menu.Dropdown>
+                          </Menu>
+                        ) : (
+                          <Switch
+                            size="xs"
+                            className="absolute top-3 right-3"
+                            defaultChecked
+                            // checked={sec.isActive}
+                          />
+                        )}
                       </div>
                     </div>
-
+                    <div className="bg-[#e7eaff] flex h-8 items-center justify-between rounded-b-[4px]">
+                      <p className="p-2.5 text-secondary font-semibold text-[12px]">
+                        {(sec.assignments?.length ?? 0) === 1
+                          ? "Assignment"
+                          : (sec.assignments?.length ?? 0) > 1
+                          ? "Assignments"
+                          : "No Assignment"}
+                      </p>
+                    </div>
                     <ModalEditSec
                       opened={openModalEditSec}
                       onClose={closeModalEditSec}
@@ -263,7 +257,7 @@ export default function Course() {
                         </p>
                       }
                     />
-                  </>
+                  </div>
                 );
               })}
             </div>
--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -55,9 +55,9 @@ export default function Dashboard() {
       const acaYear = academicYear.find(
         (e) => e.id == yearId && e.semester == semester && e.year == year
       );
-      if (acaYear && yearId != term?.id && !course.length) {
+      if (acaYear && yearId != term?.id) {
         setTerm(acaYear);
-        fetchCourse(acaYear.id);
+        if (!course.length) fetchCourse(acaYear.id);
       }
     }
   }, [academicYear, term]);
@@ -127,7 +127,7 @@ export default function Dashboard() {
   };
 
   return (
-    <div className="bg-[#ffffff] flex flex-col h-full w-full p-6 py-3 gap-3 overflow-hidden">
+    <>
       <MainPopup
         opened={openMainPopup}
         onClose={closeMainPopup}
@@ -149,178 +149,180 @@ export default function Dashboard() {
         fetchCourse={(id) => fetchCourse(id)}
       />
       <ModalEditCourse
-        // key={editCourse?.id ?? undefined}
         opened={openModalEditCourse}
         onClose={closeModalEditCourse}
         value={editCourse}
       />
 
-      <div className="flex flex-row  items-center justify-between">
-        <div className="flex flex-col">
-          <p className="text-secondary text-[20px] font-semibold mb-[1px]">
-            Hi there, {user.firstNameEN}
-          </p>
-          <p className="text-[#575757] text-[14px]">
-            In semester {term?.semester ?? ""}, {term?.year ?? ""}!{" "}
-            {course.length === 0 ? (
-              <span>Your course card is currently empty</span>
-            ) : (
-              <span>
-                Your currently have{" "}
-                <span className="text-[#5768D5] font-semibold">
-                  {totalCourses} Course
-                  {totalCourses > 1 ? "s " : " "}
+      <div className="bg-[#ffffff] flex flex-col h-full w-full p-6 py-3 gap-3 overflow-hidden">
+        <div className="flex flex-row  items-center justify-between">
+          <div className="flex flex-col">
+            <p className="text-secondary text-[20px] font-semibold mb-[1px]">
+              Hi there, {user.firstNameEN}
+            </p>
+            <p className="text-[#575757] text-[14px]">
+              In semester {term?.semester ?? ""}, {term?.year ?? ""}!{" "}
+              {course.length === 0 ? (
+                <span>Your course card is currently empty</span>
+              ) : (
+                <span>
+                  Your currently have{" "}
+                  <span className="text-[#5768D5] font-semibold">
+                    {totalCourses} Course
+                    {totalCourses > 1 ? "s " : " "}
+                  </span>
+                  on your plate.
                 </span>
-                on your plate.
-              </span>
-            )}
-          </p>
-        </div>
-        {term?.isActive && !!course.length && (
-          <Button
-            size="xs"
-            color="#5768D5"
-            leftSection={<IconPlus className=" size-5 -mr-1" stroke={1.5} />}
-            className=" rounded-[8px] text-[12px] h-[32px] w-fit "
-            onClick={openedAddModal}
-          >
-            Add Course
-          </Button>
-        )}
-      </div>
-      <div className="flex h-full w-full bg-white rounded-[5px]  overflow-hidden">
-        {loading ? (
-          <Loading />
-        ) : course.length === 0 ? (
-          <div className=" flex flex-row flex-1 justify-between">
-            <div className="h-full px-[60px] justify-center flex flex-col">
-              <p className="text-primary text-h2 font-semibold">
-                No course found
-              </p>
-              <br />
-              <p className=" -mt-4 mb-6  text-black text-b2 break-words font-400 leading-relaxed">
-                It looks like you haven't added any courses yet. <br />
-                Click 'Add Course' button below to get started!
-              </p>
-              {term?.isActive && (
-                <Button
-                  color="#5768D5"
-                  className=" rounded-[8px] text-[12px] w-28 font-medium  h-8 px-2 "
-                  onClick={openedAddModal}
-                >
-                  <IconPlus
-                    className="h-5 w-5 mr-1"
-                    stroke={1.5}
-                    color="#ffffff"
-                  />
-                  Add course
-                </Button>
               )}
-            </div>
-            <div className="h-full px-[60px] bg-slate-300  justify-center flex flex-col">
-              <img src={notFoundImage} alt="notFound"></img>
-            </div>
+            </p>
           </div>
-        ) : (
-          <InfiniteScroll
-            dataLength={course.length}
-            next={onShowMore}
-            height={"100%"}
-            loader={<Loading />}
-            hasMore={payload?.hasMore}
-            className="overflow-y-auto w-full h-fit max-h-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4 p-1"
-            style={{ height: "fit-content", maxHeight: "100%" }}
-          >
-            {course.map((item) => {
-              const statusTQF3 = statusColor(item.TQF3?.status);
-              const statusTQF5 = statusColor(item.TQF5?.status);
-              return (
-                <div
-                  key={item.id}
-                  className="card relative justify-between xl:h-[135px] md:h-[120px] cursor-pointer rounded-[4px] hover:bg-[#F3F3F3]"
-                  onClick={() => goToCourse(item.courseNo)}
-                >
-                  <div className="p-2.5 flex flex-col">
-                    <p className="font-semibold text-sm">{item.courseNo}</p>
-                    <p className="text-xs font-medium text-gray-600">
-                      {item.courseName}
-                    </p>
-                    {item.addFirstTime && term?.isActive && (
-                      <div
-                        onClick={(e) => {
-                          e.stopPropagation();
-                        }}
-                      >
-                        <Menu trigger="click" position="bottom-end" offset={2}>
-                          <Menu.Target>
-                            <IconDots className="absolute top-2 right-2 rounded-full hover:bg-gray-300" />
-                          </Menu.Target>
-                          <Menu.Dropdown
-                            className="rounded-md backdrop-blur-xl bg-white/70 "
-                            style={{
-                              boxShadow: "0px 0px 4px 0px rgba(0, 0, 0, 0.25)",
-                            }}
+          {term?.isActive && !!course.length && (
+            <Button
+              size="xs"
+              color="#5768D5"
+              leftSection={<IconPlus className=" size-5 -mr-1" stroke={1.5} />}
+              className=" rounded-[8px] text-[12px] h-[32px] w-fit "
+              onClick={openedAddModal}
+            >
+              Add Course
+            </Button>
+          )}
+        </div>
+        <div className="flex h-full w-full bg-white rounded-[5px]  overflow-hidden">
+          {loading ? (
+            <Loading />
+          ) : course.length === 0 ? (
+            <div className=" flex flex-row flex-1 justify-between">
+              <div className="h-full px-[60px] justify-center flex flex-col">
+                <p className="text-primary text-h2 font-semibold">
+                  No course found
+                </p>
+                <br />
+                <p className=" -mt-4 mb-6  text-black text-b2 break-words font-400 leading-relaxed">
+                  It looks like you haven't added any courses yet. <br />
+                  Click 'Add Course' button below to get started!
+                </p>
+                {term?.isActive && (
+                  <Button
+                    color="#5768D5"
+                    className=" rounded-[8px] text-[12px] w-28 font-medium  h-8 px-2 "
+                    onClick={openedAddModal}
+                  >
+                    <IconPlus
+                      className="h-5 w-5 mr-1"
+                      stroke={1.5}
+                      color="#ffffff"
+                    />
+                    Add course
+                  </Button>
+                )}
+              </div>
+              <div className="h-full px-[60px] bg-slate-300  justify-center flex flex-col">
+                <img src={notFoundImage} alt="notFound"></img>
+              </div>
+            </div>
+          ) : (
+            <InfiniteScroll
+              dataLength={course.length}
+              next={onShowMore}
+              height={"100%"}
+              loader={<Loading />}
+              hasMore={payload?.hasMore}
+              className="overflow-y-auto w-full h-fit max-h-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4 p-1"
+              style={{ height: "fit-content", maxHeight: "100%" }}
+            >
+              {course.map((item) => {
+                const statusTQF3 = statusColor(item.TQF3?.status);
+                const statusTQF5 = statusColor(item.TQF5?.status);
+                return (
+                  <div
+                    key={item.id}
+                    className="card relative justify-between xl:h-[135px] md:h-[120px] cursor-pointer rounded-[4px] hover:bg-[#F3F3F3]"
+                    onClick={() => goToCourse(item.courseNo)}
+                  >
+                    <div className="p-2.5 flex flex-col">
+                      <p className="font-semibold text-sm">{item.courseNo}</p>
+                      <p className="text-xs font-medium text-gray-600">
+                        {item.courseName}
+                      </p>
+                      {item.addFirstTime && term?.isActive && (
+                        <div onClick={(event) => event.stopPropagation()}>
+                          <Menu
+                            trigger="click"
+                            position="bottom-end"
+                            offset={2}
                           >
-                            <Menu.Item
-                              onClick={() => {
-                                setEditCourse({
-                                  id: item.id,
-                                  courseNo: item.courseNo,
-                                  courseName: item.courseName,
-                                });
-                                openedModalEditCourse();
-                              }}
-                              className="text-[#3E3E3E] font-semibold  text-b3 h-7 w-[180px]"
-                            >
-                              <div className="flex items-center gap-2">
-                                <IconPencilMinus
-                                  stroke={1.5}
-                                  className="h-4 w-4"
-                                />
-                                <span>Edit Course</span>
-                              </div>
-                            </Menu.Item>
-                            <Menu.Item
-                              className="text-[#FF4747] h-7 w-[180px] font-semibold text-b3 hover:bg-[#d55757]/10"
-                              onClick={() => {
-                                setDelCourse(item);
-                                openedMainPopup();
+                            <Menu.Target>
+                              <IconDots className="absolute top-2 right-2 rounded-full hover:bg-gray-300" />
+                            </Menu.Target>
+                            <Menu.Dropdown
+                              className="rounded-md backdrop-blur-xl bg-white/70 "
+                              style={{
+                                boxShadow:
+                                  "0px 0px 4px 0px rgba(0, 0, 0, 0.25)",
                               }}
                             >
-                              <div className="flex items-center gap-2">
-                                <IconTrash className="h-4 w-4" stroke={1.5} />
-                                <span>Delete Course</span>
-                              </div>
-                            </Menu.Item>
-                          </Menu.Dropdown>
-                        </Menu>
+                              <Menu.Item
+                                onClick={() => {
+                                  setEditCourse({
+                                    id: item.id,
+                                    courseNo: item.courseNo,
+                                    courseName: item.courseName,
+                                  });
+                                  openedModalEditCourse();
+                                }}
+                                className="text-[#3E3E3E] font-semibold  text-b3 h-7 w-[180px]"
+                              >
+                                <div className="flex items-center gap-2">
+                                  <IconPencilMinus
+                                    stroke={1.5}
+                                    className="h-4 w-4"
+                                  />
+                                  <span>Edit Course</span>
+                                </div>
+                              </Menu.Item>
+                              <Menu.Item
+                                className="text-[#FF4747] h-7 w-[180px] font-semibold text-b3 hover:bg-[#d55757]/10"
+                                onClick={() => {
+                                  setDelCourse(item);
+                                  openedMainPopup();
+                                }}
+                              >
+                                <div className="flex items-center gap-2">
+                                  <IconTrash className="h-4 w-4" stroke={1.5} />
+                                  <span>Delete Course</span>
+                                </div>
+                              </Menu.Item>
+                            </Menu.Dropdown>
+                          </Menu>
+                        </div>
+                      )}
+                    </div>
+                    <div className="bg-[#e7eaff] flex h-8 items-center justify-between rounded-b-[4px]">
+                      <p className="p-2.5 text-secondary font-semibold text-[12px]">
+                        {item.sections.length} Section
+                        {item.sections.length > 1 ? "s" : ""}
+                      </p>
+                      <div className="flex gap-3 px-2.5 font-semibold text-[11px] py-1 justify-end items-center">
+                        <p
+                          className={`px-1 border-[1px] rounded-md ${statusTQF3}`}
+                        >
+                          TQF 3
+                        </p>
+                        <p
+                          className={`px-1 border-[1px] rounded-md ${statusTQF5}`}
+                        >
+                          TQF 5
+                        </p>
                       </div>
-                    )}
-                  </div>
-                  <div className="bg-[#e7eaff] flex h-8 items-center justify-between rounded-b-[4px]">
-                    <p className="p-2.5 text-secondary font-semibold text-[12px]">
-                      {item.sections.length} Section
-                      {item.sections.length > 1 ? "s" : ""}
-                    </p>
-                    <div className="flex gap-3 px-2.5 font-semibold text-[11px] py-1 justify-end items-center">
-                      <p
-                        className={`px-1 border-[1px] rounded-md ${statusTQF3}`}
-                      >
-                        TQF 3
-                      </p>
-                      <p
-                        className={`px-1 border-[1px] rounded-md ${statusTQF5}`}
-                      >
-                        TQF 5
-                      </p>
                     </div>
                   </div>
-                </div>
-              );
-            })}
-          </InfiniteScroll>
-        )}
+                );
+              })}
+            </InfiniteScroll>
+          )}
+        </div>
       </div>
-    </div>
+    </>
   );
 }
