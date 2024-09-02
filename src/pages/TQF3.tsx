ace95d0d1bf7d31040e93fdf6ef5e59edda609e0
auswitch <90751246+auswitch@users.noreply.github.com>
2024-09-02 23:42:17 +0700

    add tqf3pt.1

--- src/pages/TQF3.tsx
+++ src/pages/TQF3.tsx
@@ -16,6 +16,7 @@ import {
   ScrollArea,
   Table,
   Tabs,
+  Textarea,
   TextInput,
   Tooltip,
 } from "@mantine/core";
@@ -57,16 +58,16 @@ export default function TQF3() {
 
   return (
     <>
-      <div className=" flex flex-col h-full gap-3 w-full pt-3  overflow-hidden">
+      <div className=" flex flex-col bg-[#f8f8f8] h-full gap-3 w-full overflow-hidden">
         <Tabs
           value={tqf3Part}
           onChange={setTqf3Part}
           defaultValue="tqf3p1"
           variant="pills"
-          className="px-6"
+          className="px-6 pt-4 flex flex-col  h-full "
         >
-          <div className="flex items-center justify-between">
-            <div className=" text-secondary font-semibold  whitespace-break-spaces">
+          <div className="flex items-center w-full h-fit justify-between">
+            <div className=" text-secondary  overflow-y-auto font-semibold  whitespace-break-spaces">
               {topicPart()}
             </div>
             <div className="flex gap-2">
@@ -129,77 +130,207 @@ export default function TQF3() {
               </Tooltip>
             </div>
           </div>
-          <div>
-            <Tabs.Panel value="tqf3p1">
-              <div className="flex  w-full  justify-between    pt-6  flex-1">
-                <div className="flex gap-4 flex-col">
-                  <div
-                    style={{
-                      boxShadow: "0px 0px 4px 0px rgba(0, 0, 0, 0.25)",
-                      overflowY: "auto",
-                    }}
-                    className="w-full h-fit items-center rounded-md gap-6  grid grid-cols-2 p-5 "
-                  >
-                    <div className="flex text-secondary pl-10  flex-col">
-                      <p className="font-semibold">ประเภทกระบวนวิชา</p>
-                      <p className="font-bold">Course Type</p>{" "}
+          <div
+            style={{
+              boxShadow: "0px 0px 4px 0px rgba(0, 0, 0, 0.25)",
+              overflowY: "auto",
+            }}
+            className=" h-full w-full bg-white flex mt-2 mb-4 px-5 py-2 rounded-md text-[14px] "
+          >
+            <Tabs.Panel className="w-full" value="tqf3p1">
+              <div className="flex w-full justify-start  max-h-full">
+                <div className="flex  w-full flex-col">
+                  <div className="w-full border-b-[1px] border-[#e6e6e6] justify-between h-fit  items-top  grid grid-cols-3 py-5  ">
+                    <div className="flex text-secondary pl-6  flex-col">
+                      <p className="font-medium">ประเภทกระบวนวิชา <span className=" text-red-500">*</span></p>
+                      <p className="font-semibold">Course Type</p>{" "}
                     </div>
 
                     <div className="flex text-[#333333] gap-3  flex-col">
                       {Object.keys(COURSE_TYPE).map((key) => (
                         <Radio
-                          classNames={{ label: "font-medium" }}
+                          classNames={{ label: "font-medium text-[13px]" }}
                           label={key}
                         />
                       ))}
                     </div>
                   </div>
-                  <div
-                    style={{
-                      boxShadow: "0px 0px 4px 0px rgba(0, 0, 0, 0.25)",
-                      overflowY: "auto",
-                    }}
-                    className="w-full h-fit items-center rounded-md gap-6  grid grid-cols-2 p-5 "
-                  >
-                    <div className="flex text-secondary pl-10 flex-col">
-                      <p className="font-medium">ลักษณะของกระบวนวิชา</p>
+                  <div className=" border-b-[1px] border-[#e6e6e6] justify-between h-fit w-full  items-top  grid grid-cols-3 py-5   ">
+                    <div className="flex text-secondary pl-6 flex-col ">
+                      <p className="font-medium">ลักษณะของกระบวนวิชา <span className=" text-red-500">*</span></p>
                       <p className="font-semibold">Teachig Method</p>
                     </div>
                     <div className="flex text-[#333333] gap-4  flex-col">
                       {Object.keys(TEACHING_METHOD).map((key) => (
                         <Checkbox
-                          classNames={{ label: "font-medium" }}
+                          classNames={{ label: "font-medium text-[13px]" }}
                           label={key}
                         />
                       ))}
                     </div>
                   </div>
-                </div>
-                <div className="flex flex-col">
-                  <div
-                    style={{
-                      boxShadow: "0px 0px 4px 0px rgba(0, 0, 0, 0.25)",
-                      overflowY: "auto",
-                    }}
-                    className="w-full h-fit items-center rounded-md gap-12 justify-start flex p-6"
-                  >
-                    <div className="flex text-secondary gap-2  flex-row">
-                      <p className="font-semibold">ประเภทกระบวนวิชา</p>
-                      <p className="font-bold">(Course Type)</p>
+                  <div className="w-full border-b-[1px] border-[#e6e6e6] justify-between h-fit  items-top  grid grid-cols-3 py-5  ">
+                    <div className="flex text-secondary pl-6 pt-2 flex-col">
+                      <p className="font-medium">ชั้นปีที่เรียน <span className=" text-red-500">*</span></p>
+                      <p className="font-semibold">Student Year</p>
                     </div>
-                    <div className="flex text-[#333333] gap-4  flex-col">
+
+                    <div className="flex gap-8 text-[#333333]">
+                      <div className="flex flex-col gap-5">
+                        <Checkbox
+                          classNames={{ label: "font-medium text-[13px]" }}
+                          label="ชั้นปีที่ 1 (1st year)"
+                        />
+                        <Checkbox
+                          classNames={{ label: "font-medium text-[13px]" }}
+                          label="ชั้นปีที่ 2 (2nd year)"
+                        />{" "}
+                        <Checkbox
+                          classNames={{ label: "font-medium text-[13px]" }}
+                          label="ชั้นปีที่ 3 (3rd year)"
+                        />
+                      </div>
+                      <div className="flex flex-col gap-5">
+                        <Checkbox
+                          classNames={{ label: "font-medium text-[13px]" }}
+                          label="ชั้นปีที่ 4 (4th year)"
+                        />
+                        <Checkbox
+                          classNames={{ label: "font-medium text-[13px]" }}
+                          label="ชั้นปีที่ 5 (5th year)"
+                        />{" "}
+                        <Checkbox
+                          classNames={{ label: "font-medium text-[13px]" }}
+                          label="ชั้นปีที่ 6 (6th year)"
+                        />
+                      </div>
+                    </div>
+                  </div>
+                  <div className="w-full border-b-[1px] border-[#e6e6e6] justify-between h-fit  items-center  grid grid-cols-3 py-5  ">
+                    <div className="flex text-secondary pl-6 flex-col">
+                      <p className="font-medium">การวัดและประเมินผล <span className=" text-red-500">*</span></p>
+                      <p className="font-semibold">Evaluation</p>
+                    </div>
+
+                    <div className="flex gap-8 text-[#333333]">
                       <Radio
-                        classNames={{ label: "font-medium" }}
-                        label="วิชาศึกษาทั่วไป (General Education)"
+                        classNames={{ label: "font-medium text-[13px]" }}
+                        label="A-F"
                       />
                       <Radio
-                        classNames={{ label: "font-medium" }}
-                        label="วิชาเฉพาะ (Field of specialization)"
-                      />
+                        classNames={{ label: "font-medium text-[13px]" }}
+                        label="S/U"
+                      />{" "}
                       <Radio
-                        classNames={{ label: "font-medium" }}
-                        label="วิชาเลือกเสรี (Free elective)"
+                        classNames={{ label: "font-medium text-[13px]" }}
+                        label="P"
                       />
+                    </div>
+                  </div>
+                  <div className="w-full border-b-[1px] border-[#e6e6e6] justify-between h-fit  items-top  grid grid-cols-3 py-5  ">
+                    <div className="flex text-secondary pl-6 flex-col">
+                      <p className="font-medium">อาจารย์ผู้สอนทั้งหมด<span className=" text-red-500">*</span></p>
+                      <p className="font-semibold">Lecturers</p>
+                    </div>
+
+                    <div className="flex flex-col gap-3 text-[#333333]">
+                      <TextInput
+                        withAsterisk
+                        size="xs"
+                        label="Instructor 1"
+                        classNames={{ label: "text-[#333333]" }}
+                        className="w-[440px]"
+                        placeholder="(required)"
+                      />
+                      <TextInput
+                        label="Instructor 2"
+                        size="xs"
+                        classNames={{ label: "text-[#333333]" }}
+                        className="w-[440px]"
+                        placeholder="(optional)"
+                      />
+                      <TextInput
+                        label="Instructor 3"
+                        size="xs"
+                        classNames={{ label: "text-[#333333]" }}
+                        className="w-[440px]"
+                        placeholder="(optional)"
+                      />
+                      <TextInput
+                        label="Instructor 4"
+                        size="xs"
+                        classNames={{ label: "text-[#333333]" }}
+                        className="w-[440px]"
+                        placeholder="(optional)"
+                      />
+                      <TextInput
+                        label="Instructor 5"
+                        size="xs"
+                        classNames={{ label: "text-[#333333]" }}
+                        className="w-[440px]"
+                        placeholder="(optional)"
+                      />
+                      <TextInput
+                        label="Instructor 6"
+                        size="xs"
+                        classNames={{ label: "text-[#333333]" }}
+                        className="w-[440px]"
+                        placeholder="(optional)"
+                      />
+                      <TextInput
+                        label="Instructor 7"
+                        size="xs"
+                        classNames={{ label: "text-[#333333]" }}
+                        className="w-[440px]"
+                        placeholder="(optional)"
+                      />
+                      <TextInput
+                        label="Instructor 8"
+                        size="xs"
+                        classNames={{ label: "text-[#333333]" }}
+                        className="w-[440px]"
+                        placeholder="(optional)"
+                      />
+                    </div>
+                  </div>
+                  <div className="w-full border-b-[1px] border-[#e6e6e6] justify-between h-fit  items-top  grid grid-cols-3 py-5  ">
+                    <div className="flex text-secondary pl-6 flex-col">
+                      <p className="font-medium">สถานที่สอนคาบบรรยาย</p>
+                      <p className="font-semibold">Lectures Venue</p>
+                    </div>
+
+                    <div className="flex flex-col gap-3 text-[#333333]">
+                      <Textarea label='Description' size="xs" placeholder="(optional)" className="w-[440px]" classNames={{ input: "h-[180px] p-3"}}></Textarea>
+                    </div>
+                  </div>
+                  <div className="w-full border-b-[1px] border-[#e6e6e6] justify-between h-fit  items-top  grid grid-cols-3 py-5  ">
+                    <div className="flex text-secondary pl-6 flex-col">
+                      <p className="font-medium">สถานที่สอนคาบแลป</p>
+                      <p className="font-semibold">Laboratory Venue</p>
+                    </div>
+
+                    <div className="flex flex-col gap-3 text-[#333333]">
+                      <Textarea label='Description' size="xs" placeholder="(optional)" className="w-[440px]" classNames={{ input: "h-[180px] p-3", label: "text-[#333333]"}}></Textarea>
+                    </div>
+                  </div>
+                  <div className="w-full border-b-[1px] border-[#e6e6e6] justify-between h-fit  items-top  grid grid-cols-3 py-5  ">
+                    <div className="flex text-secondary pl-6 flex-col">
+                      <p className="font-medium">ตำราและเอกสาร</p>
+                      <p className="font-semibold">Main Reference</p>
+                    </div>
+
+                    <div className="flex flex-col gap-3 text-[#333333]">
+                      <Textarea label='Description' size="xs" placeholder="(optional)" className="w-[440px]" classNames={{ input: "h-[180px] p-3", label: "text-[#333333]"}}></Textarea>
+                    </div>
+                  </div>
+                  <div className="w-full border-b-[1px] border-[#e6e6e6] justify-between h-fit  items-top  grid grid-cols-3 py-5  ">
+                    <div className="flex text-secondary pl-6 flex-col">
+                      <p className="font-medium">เอกสารแนะนำ</p>
+                      <p className="font-semibold">Recommended Documents, e.g. Lecture notes, E-documents, etc.</p>
+                    </div>
+
+                    <div className="flex flex-col gap-3 text-[#333333]">
+                      <Textarea label='Description' size="xs" placeholder="(optional)" className="w-[440px]" classNames={{ input: "h-[180px] p-3", label: "text-[#333333]"}}></Textarea>
                     </div>
                   </div>
                 </div>
