fc89bd7bc8ee2a1bc3287837517977794185d7d6
worapit <drm.wmwork@gmail.com>
2024-09-04 01:56:09 +0700

    Planning Table & Fix Drag'n'Drop Table

--- src/components/TQF3/Part2TQF3.tsx
+++ src/components/TQF3/Part2TQF3.tsx
@@ -43,6 +43,7 @@ export default function Part2TQF3() {
       Lec: "Lecture (if any)",
       Lab: "Laboratory (if any)",
     },
+
     {
       no: 4,
       cloTH: "อธิบายหลักการทำงานของระบบปฏิบัติการคอมพิวเตอร์.",
@@ -51,18 +52,61 @@ export default function Part2TQF3() {
       Lab: "Laboratory (if any)",
     },
   ];
+  const dataPlan = [
+    {
+      no: 1,
+      Topic: "Course introduction",
+      lecHr: 1.5,
+      LabHr: 0.0,
+    },
+    {
+      no: 2,
+      Topic: "Operating-System Structures",
+      lecHr: 1.5,
+      LabHr: 0.0,
+    },
+    {
+      no: 3,
+      Topic: "Processes",
+      lecHr: 1.5,
+      LabHr: 0.0,
+    },
+    {
+      no: 4,
+      Topic: "Threads",
+      lecHr: 1.5,
+      LabHr: 0.0,
+    },
+    {
+      no: 5,
+      Topic: "Process Synchronization",
+      lecHr: 1.5,
+      LabHr: 0.0,
+    },
+  ];
   const [state, handlers] = useListState(dataTest);
+  const [statePlan, handlersPlan] = useListState(dataPlan);
+
+  useEffect(() => {
+    if (dataPlan) {
+      handlersPlan.setState(dataPlan);
+    }
+  }, [dataPlan]);
 
   useEffect(() => {
     if (dataTest) {
       handlers.setState(dataTest);
     }
   }, [dataTest]);
+
   return (
-    <div className="flex w-full max-h-full">
-      <div className="flex flex-col border-b-[1px] w-full border-[#e6e6e6]  gap-5">
+    <div className="flex flex-col w-full max-h-full gap-5">
+      {/* Description */}
+      <div className="flex flex-col border-b-[1px] w-full border-[#e6e6e6]  gap-5 pb-5">
         <div className="flex text-secondary items-center w-full justify-between">
-          <p className="font-medium">วัตถุประสงค์ของกระบวนวิชา (CLO)</p>
+          <p className="font-semibold">
+            วัตถุประสงค์ของกระบวนวิชา <span className="font-bold">(CLO)</span>
+          </p>
           <Button
             className="rounded-[8px] text-[12px] w-fit font-medium h-8 px-4"
             // onClick={() => setOpenAddModal(true)}
@@ -95,7 +139,7 @@ export default function Part2TQF3() {
         {/* Table */}
         <DragDropContext
           onDragEnd={({ destination, source }) => {
-            if (!destination) return; // Check if destination exists
+            if (!destination) return;
             handlers.reorder({
               from: source.index,
               to: destination.index,
@@ -108,12 +152,12 @@ export default function Part2TQF3() {
           >
             <Table stickyHeader striped className="w-full">
               <Table.Thead>
-                <Table.Tr className="bg-[#F4F5FE]">
+                <Table.Tr className="bg-[#e5e7f6]">
                   <Table.Th className="w-[10%]">CLO No.</Table.Th>
-                  <Table.Th className="w-[40%]">CLO Description</Table.Th>
+                  <Table.Th className="w-[50%]">CLO Description</Table.Th>
                   <Table.Th className="w-[20%]">Learning Method</Table.Th>
-                  <Table.Th className="w-[20%]">Action</Table.Th>
-                  <Table.Th className="w-[10%]"></Table.Th>
+                  <Table.Th className="w-[15%]">Action</Table.Th>
+                  <Table.Th className="w-[5%]"></Table.Th>
                 </Table.Tr>
               </Table.Thead>
 
@@ -122,7 +166,7 @@ export default function Part2TQF3() {
                   <Table.Tbody
                     ref={provided.innerRef}
                     {...provided.droppableProps}
-                    className="text-default text-b3 w-full"
+                    className="text-default text-[13px] font-medium w-full"
                   >
                     {state.map((item, index) => (
                       <Draggable
@@ -137,17 +181,21 @@ export default function Part2TQF3() {
                             className={`table-row ${
                               snapshot.isDragging ? "bg-hover " : ""
                             }`}
-                            // style={{
-                            //   ...provided.draggableProps.style,
-                            //   display: "table-row",
-                            // }}
                           >
                             <Table.Td className="w-[10%]">{item.no}</Table.Td>
-                            <Table.Td className="w-[40%]">
-                              {item.cloTH}
+                            <Table.Td className="w-[50%]">
+                              <div className="flex flex-col gap-0.5">
+                                <p>{item.cloTH}</p>
+                                <p>{item.cloEN}</p>
+                              </div>
                             </Table.Td>
-                            <Table.Td className="w-[20%]">{item.Lab}</Table.Td>
                             <Table.Td className="w-[20%]">
+                              <div className="flex flex-col gap-0.5">
+                                <p>{item.Lec}</p>
+                                <p>{item.Lab}</p>
+                              </div>
+                            </Table.Td>
+                            <Table.Td className="w-[15%]">
                               <div className="flex justify-start gap-4 items-center">
                                 <div className="flex justify-center items-center bg-transparent border-[1px] border-[#F39D4E] text-[#F39D4E] size-8 bg-none rounded-full cursor-pointer hover:bg-[#F39D4E]/10">
                                   <IconEdit className="size-4" stroke={1.5} />
@@ -159,11 +207,11 @@ export default function Part2TQF3() {
                             </Table.Td>
                             <Table.Td
                               className={`${
-                                snapshot.isDragging ? "w-[200px]" : ""
+                                snapshot.isDragging ? "w-[5%]" : ""
                               }`}
                             >
                               <div
-                                className="cursor-pointer hover:bg-hover text-tertiary size-8 rounded-full flex items-center justify-center "
+                                className="cursor-pointer hover:bg-hover text-tertiary size-8 rounded-full flex items-center justify-center"
                                 {...provided.dragHandleProps}
                               >
                                 <IconGripVertical
@@ -183,6 +231,132 @@ export default function Part2TQF3() {
                   </Table.Tbody>
                 )}
               </Droppable>
+            </Table>
+          </div>
+        </DragDropContext>
+      </div>
+
+      {/* Planning */}
+      <div className="flex flex-col border-b-[1px] w-full border-[#e6e6e6]  gap-5 pb-5">
+        <div className="flex text-secondary items-center w-full justify-between">
+          <p className="font-semibold">
+            เนื้อหาวิชาและแผนการสอน
+            <span className="font-bold"> (Course content and Schedule)</span>
+          </p>
+          <Button
+            className="rounded-[8px] text-[12px] w-fit font-medium h-8 px-4"
+            // onClick={() => setOpenAddModal(true)}
+          >
+            <IconPlus className="h-5 w-5 mr-1" stroke={1.5} color="#ffffff" />
+            Add Topic
+          </Button>
+        </div>
+        {/* Table */}
+        <DragDropContext
+          onDragEnd={({ destination, source }) => {
+            if (!destination) return;
+            handlers.reorder({
+              from: source.index,
+              to: destination.index,
+            });
+          }}
+        >
+          <div
+            className="overflow-y-auto overflow-x-auto w-full h-fit max-h-full border flex flex-col rounded-lg border-secondary"
+            style={{ height: "fit-content" }}
+          >
+            <Table stickyHeader striped className="w-full">
+              <Table.Thead>
+                <Table.Tr className="bg-[#e5e7f6]">
+                  <Table.Th className="w-[10%] ">Week No.</Table.Th>
+                  <Table.Th className="w-[30%]">Topic</Table.Th>
+                  <Table.Th className="w-[20%] text-end">Lecture Hour</Table.Th>
+                  <Table.Th className="w-[20%] text-end !pr-24">
+                    Lab Hour
+                  </Table.Th>
+                  <Table.Th className="w-[15%]">Action</Table.Th>
+                  <Table.Th className="w-[5%]"></Table.Th>
+                </Table.Tr>
+              </Table.Thead>
+
+              <Droppable droppableId="dnd-list" direction="vertical">
+                {(provided) => (
+                  <Table.Tbody
+                    ref={provided.innerRef}
+                    {...provided.droppableProps}
+                    className="text-default text-[13px] font-medium w-full"
+                  >
+                    {statePlan.map((item, index) => (
+                      <Draggable
+                        key={item.no.toString()}
+                        index={index}
+                        draggableId={item.no.toString()}
+                      >
+                        {(provided, snapshot) => (
+                          <Table.Tr
+                            ref={provided.innerRef}
+                            {...provided.draggableProps}
+                            className={`table-row ${
+                              snapshot.isDragging ? "bg-hover " : ""
+                            }`}
+                          >
+                            <Table.Td className="w-[10%]">{item.no}</Table.Td>
+                            <Table.Td className="w-[30%]">
+                              <p>{item.Topic}</p>
+                            </Table.Td>
+                            <Table.Td className="w-[20%] text-end">
+                              <p>{item.lecHr}</p>
+                            </Table.Td>
+                            <Table.Td className="w-[20%] text-end !pr-24">
+                              <p>{item.LabHr}</p>
+                            </Table.Td>
+                            <Table.Td className="w-[15%]">
+                              <div className="flex justify-start gap-4 items-center">
+                                <div className="flex justify-center items-center bg-transparent border-[1px] border-[#F39D4E] text-[#F39D4E] size-8 bg-none rounded-full cursor-pointer hover:bg-[#F39D4E]/10">
+                                  <IconEdit className="size-4" stroke={1.5} />
+                                </div>
+                                <div className="flex justify-center items-center bg-transparent border-[1px] size-8 bg-none rounded-full cursor-pointer border-[#FF4747] text-[#FF4747] hover:bg-[#FF4747]/10">
+                                  <IconTrash className="size-4" stroke={1.5} />
+                                </div>
+                              </div>
+                            </Table.Td>
+                            <Table.Td
+                              className={`${
+                                snapshot.isDragging ? "w-[5%]" : ""
+                              }`}
+                            >
+                              <div
+                                className="cursor-pointer hover:bg-hover text-tertiary size-8 rounded-full flex items-center justify-center"
+                                {...provided.dragHandleProps}
+                              >
+                                <IconGripVertical
+                                  style={{
+                                    width: "20px",
+                                    height: "20px",
+                                  }}
+                                  stroke={1.5}
+                                />
+                              </div>
+                            </Table.Td>
+                          </Table.Tr>
+                        )}
+                      </Draggable>
+                    ))}
+                    {provided.placeholder}
+                  </Table.Tbody>
+                )}
+              </Droppable>
+
+              <Table.Tfoot className="text-secondary border-t border-[#DEE2E6] font-semibold !h-[10px] ">
+                <Table.Tr className="bg-[#e5e7f6]">
+                  <Table.Th className="w-[10%] ">Total</Table.Th>
+                  <Table.Th className="w-[30%]"></Table.Th>
+                  <Table.Th className="w-[20%] text-end">7.5</Table.Th>
+                  <Table.Th className="w-[20%] text-end !pr-24">0.0</Table.Th>
+                  <Table.Th className="w-[15%]"></Table.Th>
+                  <Table.Th className="w-[5%]"></Table.Th>
+                </Table.Tr>
+              </Table.Tfoot>
             </Table>
           </div>
         </DragDropContext>
