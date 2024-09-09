ff6a52a4b9298ed270dd531fc8dd923a58878f7f
auswitch <90751246+auswitch@users.noreply.github.com>
2024-09-09 12:01:03 +0700

    edit compoTopic TQF3 Part6

--- src/components/Modal/TQF3/ModalManageTopic.tsx
+++ src/components/Modal/TQF3/ModalManageTopic.tsx
@@ -11,7 +11,12 @@ import {
   Select,
 } from "@mantine/core";
 import { useForm } from "@mantine/form";
-import { IconMinus, IconPlus, IconTrash } from "@tabler/icons-react";
+import {
+  IconArrowRight,
+  IconMinus,
+  IconPlus,
+  IconTrash,
+} from "@tabler/icons-react";
 import { log } from "console";
 import { upperFirst } from "lodash";
 import { useEffect, useRef, useState } from "react";
@@ -59,7 +64,7 @@ export default function ModalManageTopic({
   ];
 
   const form = useForm({
-    mode: "uncontrolled",
+    mode: "controlled",
     initialValues: {} as Partial<IModelTQF3Part6>,
     validate: {},
   });
@@ -74,25 +79,26 @@ export default function ModalManageTopic({
       opened={opened}
       onClose={onCloseModal}
       closeOnClickOutside={false}
-      withCloseButton={false}
-      title={`${upperFirst(type)} Topic 261405`}
+      title={`${upperFirst(type)} Topic TQF3 Part 6 261405`}
       size="40vw"
       centered
       transitionProps={{ transition: "pop" }}
       classNames={{
-        content: `flex flex-col bg-[#F6F7FA] overflow-hidden `,
-        body: `overflow-hidden ${type === "add" ? "h-full" : "h-fit"}`,
+        content: "flex flex-col overflow-hidden pb-2  max-h-full h-fit",
+        body: `${
+          type === "add" ? "h-full" : "h-fit"
+        } flex flex-col overflow-hidden max-h-full h-fit`,
       }}
     >
       <div
-        className={`flex flex-col !gap-5 ${
+        className={`flex flex-col !gap-8 ${
           type === "add" ? "h-full" : "h-fit"
         } `}
       >
         {/* Input Field */}
-        <div className={`flex h-fit mb-5 flex-col gap-4`}>
+        <div className={`flex flex-col h-fit mb-6`}>
           <Select
-            size="sm"
+            size="xs"
             label="Select Topic"
             placeholder="Topic"
             data={options.map((item) => ({
@@ -100,7 +106,7 @@ export default function ModalManageTopic({
               label: `${" "}${item.th}\n${" "}${item.en}`,
             }))}
             classNames={{
-              option: "text-[13px] py-2 px-2 ",
+              option: "text-[13px] py-2 px-2",
               options: "whitespace-break-spaces leading-5",
               input: "whitespace-break-spaces flex flex-col flex-wrap",
             }}
@@ -108,48 +114,58 @@ export default function ModalManageTopic({
           />
 
           {form.getValues().topic && (
-            <>
-              <Textarea
-                autoFocus={false}
-                label={
-                  <p className="font-semibold flex gap-1">
-                    Description
-                    <span className=" text-error">*</span>
-                  </p>
-                }
-                className="w-full border-none rounded-r-none"
-                classNames={{
-                  input: "flex h-[150px] py-2 px-3 text-[13px]",
-                  label: "flex pb-1",
-                }}
-                placeholder="Ex. ใช้แบบสอบถามความพึงพอใจ"
-                onChange={(event) => {
-                  form.setFieldValue("detail", [event.target.value]);
-                }}
-              />
-            </>
+            <Textarea
+              autoFocus={false}
+              label={
+                <p className="font-semibold flex gap-1">
+                  Description
+                  <span className=" text-error">*</span>
+                </p>
+              }
+              className="w-full border-none mt-3 rounded-r-none"
+              classNames={{
+                input: "flex h-[150px] py-2 px-3 text-[13px]",
+                label: "flex pb-1",
+              }}
+              placeholder="Ex. ใช้แบบสอบถามความพึงพอใจ"
+              onChange={(event) => {
+                form.setFieldValue("detail", [event.target.value]);
+              }}
+            />
           )}
         </div>
       </div>
       {/* Button */}
-      <div className="flex gap-2  items-end  justify-end h-fit">
-        <Button
-          onClick={onCloseModal}
-          variant="subtle"
-          color="#575757"
-          className="rounded-[8px] text-[12px] h-8 w-fit "
-        >
-          Cancel
-        </Button>
-        <Button
-          onClick={() => {
-            action(form.getValues(), options);
-            onClose();
-          }}
-          className="rounded-[8px] text-[12px] h-8 w-fit "
-        >
-          Add
-        </Button>
+      <div className="flex justify-end w-full">
+        <Group className="flex w-full h-fit items-end justify-end">
+          <div>
+            <Button
+              onClick={onCloseModal}
+              color="#575757"
+              variant="subtle"
+              className="rounded-[8px] text-[12px] h-[32px] w-fit "
+            >
+              Cancel
+            </Button>
+          </div>
+          <Button
+            onClick={() => {
+              action(form.getValues(), options);
+              onClose();
+            }}
+            leftSection={
+              <IconPlus
+                color="#ffffff"
+                className="size-5 items-center"
+                stroke={2}
+                size={20}
+              />
+            }
+            className="rounded-[8px] border-none text-[12px] h-[32px] w-fit"
+          >
+            Add
+          </Button>
+        </Group>
       </div>
     </Modal>
   );
