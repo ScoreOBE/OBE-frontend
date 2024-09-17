8cc44b3892f822082f66cf7f09588177493976ac
thanaporn.ch <thanaporn_chan@cmu.ac.th>
2024-09-17 23:48:48 +0700

    #feature: Part5TQF3

--- src/components/TQF3/Part5TQF3.tsx
+++ src/components/TQF3/Part5TQF3.tsx
@@ -1,25 +1,9 @@
-import { COURSE_TYPE, TEACHING_METHOD } from "@/helpers/constants/enum";
-import {
-  Radio,
-  Checkbox,
-  TextInput,
-  Textarea,
-  Button,
-  Alert,
-  Table,
-} from "@mantine/core";
+import { Textarea } from "@mantine/core";
 import { useForm } from "@mantine/form";
-import { IconCheckbox, IconInfoCircle } from "@tabler/icons-react";
-import AddIcon from "@/assets/icons/plus.svg?react";
-import Icon from "../Icon";
-import IconPLO from "@/assets/icons/PLOdescription.svg?react";
-import DrawerPLOdes from "@/components/DrawerPLO";
-import { useEffect, useState } from "react";
 import { IModelCourse } from "@/models/ModelCourse";
-import { IModelCLO, IModelTQF3Part5 } from "@/models/ModelTQF3";
-import { IModelPLO, IModelPLOCollection, IModelPLONo } from "@/models/ModelPLO";
-import { getPLOs } from "@/services/plo/plo.service";
-import { useAppSelector } from "@/store";
+import { IModelTQF3Part5 } from "@/models/ModelTQF3";
+import { isEqual } from "lodash";
+import { useEffect } from "react";
 
 type Props = {
   data: IModelCourse;
@@ -27,98 +11,32 @@ type Props = {
 };
 
 export default function Part5TQF3({ data, setForm }: Props) {
-  const [openDrawerPLOdes, setOpenDrawerPLOdes] = useState(false);
-  const [coursePLO, setCoursePLO] = useState<Partial<IModelPLO>>();
-  const user = useAppSelector((state) => state.user);
-
-  // fixed Data
-  const courseCLO: IModelCLO[] = [
-    {
-      id: "kmskx",
-      no: 1,
-      descTH: "อธิบายหลักการทำงานของระบบปฏิบัติการคอมพิวเตอร์.1",
-      descEN:
-        "Explain the working principle of computer operating systems the working principle of computer operating systems 1",
-      learningMethod: [],
-    },
-    {
-      id: "kmskx",
-      no: 2,
-      descTH: "อธิบายหลักการทำงานของระบบปฏิบัติการคอมพิวเตอร์.2",
-      descEN:
-        "Explain the working principle of computer operating systems the working principle of computer operating systems 2",
-      learningMethod: [],
-    },
-    {
-      id: "kmskx",
-      no: 3,
-      descTH: "อธิบายหลักการทำงานของระบบปฏิบัติการคอมพิวเตอร์.3",
-      descEN:
-        "Explain the working principle of computer operating systems the working principle of computer operating systems 3",
-      learningMethod: [],
-    },
-    {
-      id: "kmskx",
-      no: 4,
-      descTH: "อธิบายหลักการทำงานของระบบปฏิบัติการคอมพิวเตอร์.4",
-      descEN:
-        "Explain the working principle of computer operating systems the working principle of computer operating systems 4",
-      learningMethod: [],
-    },
-    {
-      id: "kmskx",
-      no: 5,
-      descTH: "อธิบายหลักการทำงานของระบบปฏิบัติการคอมพิวเตอร์.5",
-      descEN:
-        "Explain the working principle of computer operating systems the working principle of computer operating systems 5",
-      learningMethod: [],
-    },
-  ];
-
   const form = useForm({
     mode: "controlled",
     initialValues: {
-      data: courseCLO.map((clo) => ({
-        mainRef: "",
-        recDoc: "",
-        clo: clo,
-        plo: [],
-      })) as Partial<IModelTQF3Part5>[],
+      mainRef: "",
+      recDoc: "",
+    } as IModelTQF3Part5,
+    onValuesChange(values, previous) {
+      if (!isEqual(values, previous)) {
+        setForm(form);
+      }
     },
-    validate: {},
   });
 
-  const fetchPLO = async () => {
-    const res = await getPLOs({
-      role: user.role,
-      departmentCode: user.departmentCode,
-    });
-
-    if (res) {
-      //fixed Data
-      const ploCol = res.plos
-        .find((dep: any) => dep.departmentCode.includes("CPE"))
-        .collections.find((col: any) => col.isActive === true);
-
-      setCoursePLO(ploCol);
-    }
-  };
-
   useEffect(() => {
-    if (data) {
-      fetchPLO();
+    if (data.TQF3?.part5) {
+      form.setValues(data.TQF3.part5);
     }
   }, [data]);
 
   return (
     <>
-      {/* Part 5 */}
       <div className="w-full border-b-[1px] border-[#e6e6e6] justify-between h-fit  items-top  grid grid-cols-3 pb-5 mt-1 ">
         <div className="flex text-secondary flex-col text-[15px]">
           <p className="font-semibold">ตำราและเอกสาร</p>
           <p className="font-semibold">Main Reference</p>
         </div>
-
         <div className="flex flex-col gap-3 text-default">
           <Textarea
             key={form.key("mainRef")}
@@ -135,9 +53,8 @@ export default function Part5TQF3({ data, setForm }: Props) {
         <div className="flex text-secondary flex-col text-[15px]">
           <p className="font-semibold">เอกสารแนะนำ</p>
           <p className="font-semibold">
-            Recommended Documents, e.g. Lecture notes,
-            <br />
-            E-documents, etc.
+            Recommended Documents
+            <br /> e.g. Lecture notes, E-documents, etc.
           </p>
         </div>
 
