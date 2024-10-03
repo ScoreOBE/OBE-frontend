599d1525ec0c8b6a3e5cb125ed0991162bee3245
auswitch <90751246+auswitch@users.noreply.github.com>
2024-10-04 01:28:04 +0700

    edit tqf3 p7

--- src/components/TQF3/Part7TQF3.tsx
+++ src/components/TQF3/Part7TQF3.tsx
@@ -99,168 +99,182 @@ export default function Part7TQF3({ setForm }: Props) {
           />
         )}
 
-      <div className="flex flex-col w-full max-h-full gap-4 -mt-1 pb-4">
-        {/* Topic */}
-        <div className="flex text-secondary items-center w-full justify-between">
-          <span className="text-[15px] font-semibold">
-            CLO Mapping
-            <span className=" text-red-500">*</span>
-          </span>
-          <Button
-            className="text-center px-4"
-            onClick={() => setOpenDrawerPLOdes(true)}
-          >
-            <div className="flex gap-2">
-              <Icon IconComponent={IconPLO} />
-              PLO Description
-            </div>
-          </Button>
-        </div>
-        <div className="w-full">
-          <Alert
-            radius="md"
-            icon={<IconCheckbox />}
-            variant="light"
-            color="rgba(6, 158, 110, 1)"
-            classNames={{
-              icon: "size-6",
-              body: " flex justify-center",
-            }}
-            className="w-full"
-            title={
-              <p className="font-semibold">
-                Each CLO must be linked to at least one PLO.
-                {ploRequired.length > 0 && (
-                  <>
-                    If you see
-                    <span className="text-red-500 font-bold">'required'</span>{" "}
-                    in a PLO column, at least one of your CLOs must be linked
-                    to that required PLO.
-                  </>
-                )}
-              </p>
-            }
-          ></Alert>
-        </div>
+        <div className="flex flex-col w-full max-h-full gap-4 -mt-1 pb-4">
+          {/* Topic */}
+          <div className="flex text-secondary items-center w-full justify-between">
+            <span className="text-[15px] font-semibold">
+              CLO Mapping
+              <span className=" text-red-500">*</span>
+            </span>
+            <Button
+              className="text-center px-4"
+              onClick={() => setOpenDrawerPLOdes(true)}
+            >
+              <div className="flex gap-2">
+                <Icon IconComponent={IconPLO} />
+                PLO Description
+              </div>
+            </Button>
+          </div>
+          <div className="w-full">
+            <Alert
+              radius="md"
+              icon={<IconCheckbox />}
+              variant="light"
+              color="rgba(6, 158, 110, 1)"
+              classNames={{
+                icon: "size-6",
+                body: " flex justify-center",
+              }}
+              className="w-full"
+              title={
+                <p className="font-semibold">
+                  Each CLO must be linked to at least one PLO.
+                  {ploRequired.length > 0 && (
+                    <>
+                      If you see
+                      <span className="text-red-500 font-bold">
+                        'required'
+                      </span>{" "}
+                      in a PLO column, at least one of your CLOs must be linked
+                      to that required PLO.
+                    </>
+                  )}
+                </p>
+              }
+            ></Alert>
+          </div>
 
-        {/* Table */}
-        <div
-          key={form.key("data")}
-          style={{
-            boxShadow: "0px 0px 4px 0px rgba(0, 0, 0, 0.25)",
-          }}
-          className=" overflow-x-auto w-full h-full max-h-full border flex flex-col rounded-lg border-secondary relative"
-        >
-          <Table stickyHeader striped>
-            <Table.Thead className="z-[2]">
-              <Table.Tr>
-                <Table.Th
-                  style={{
-                    filter: "drop-shadow(2px 0px 2px rgba(0, 0, 0, 0.1))",
-                  }}
-                  className="min-w-[500px] sticky left-0 !p-0"
-                >
-                  <div className="w-full flex items-center px-[25px] h-[58px] border-r-[1px] border-[#DEE2E6]">
-                    CLO Description ( {tqf3.part2?.clo.length} CLO
-                    {tqf3.part2?.clo.length! > 1 ? "s" : ""} )
-                  </div>
-                </Table.Th>
-                {coursePLO?.data?.map(({ no, id }) => (
+          {/* Table */}
+          <div
+            key={form.key("data")}
+            style={{
+              boxShadow: "0px 0px 4px 0px rgba(0, 0, 0, 0.25)",
+            }}
+            className=" overflow-x-auto w-full h-full max-h-full border flex flex-col rounded-lg border-secondary relative"
+          >
+            <Table stickyHeader striped>
+              <Table.Thead className="z-[2]">
+                <Table.Tr>
                   <Table.Th
-                    key={id}
-                    className="min-w-[100px] !pt-3 !pb-2 w-fit"
+                    style={{
+                      filter: "drop-shadow(2px 0px 2px rgba(0, 0, 0, 0.1))",
+                    }}
+                    className="min-w-[500px] sticky left-0 !p-0"
                   >
-                    <p className="">
-                      PLO-{no}{" "}
-                      <span className="text-red-500">
-                        {ploRequired.includes(id) && "required"}
-                      </span>
-                    </p>
-                    <p className="error-text mt-1">
-                      {validatePloRequired &&
-                        ploRequired.includes(id) &&
-                        !form
-                          .getValues()
-                          .data.some(({ plos }) =>
-                            (plos as string[]).includes(id)
-                          ) &&
-                        "Select CLO at least one"}
-                    </p>
+                    <div className="w-full flex items-center px-[25px] h-[58px] border-r-[1px] border-[#DEE2E6]">
+                      CLO Description ( {tqf3.part2?.clo.length} CLO
+                      {tqf3.part2?.clo.length! > 1 ? "s" : ""} )
+                    </div>
                   </Table.Th>
-                ))}
-              </Table.Tr>
-            </Table.Thead>
-            <Table.Tbody>
-              {form.getValues().data.map(({ clo }, cloIndex) => {
-                const cloItem = tqf3?.part2?.clo.find((e) => e.id == clo);
-                return (
-                  <Table.Tr key={cloIndex} className="text-[13px] text-default">
-                    <Table.Td
-                      key={form.key(`data.${cloIndex}.plos`)}
-                      style={{
-                        filter: "drop-shadow(2px 0px 2px rgba(0, 0, 0, 0.1))",
-                      }}
-                      className="!p-0 !py-1 sticky left-0 z-[1]"
-                      {...form.getInputProps(`data.${cloIndex}.plos`)}
+                  {coursePLO?.data?.map(({ no, id }) => (
+                    <Table.Th
+                      key={id}
+                      className="min-w-[100px] !pt-3 !pb-2 w-fit"
                     >
-                      <div className="flex gap-5 justify-start  items-center  px-[20px] py-2">
-                        <div className="text-secondary min-w-fit font-bold">
-                          CLO-{cloItem?.no}
-                        </div>
-                        <p className="flex w-fit font-medium justify-between flex-col ">
-                          <span className="mb-2">{cloItem?.descTH}</span>
-                          <span>{cloItem?.descEN}</span>
-                          <span className="error-text mt-1">
-                            {form.getInputProps(`data.${cloIndex}.plos`).error}
-                          </span>
-                        </p>
-                      </div>
-                    </Table.Td>
-                    {coursePLO?.data?.map(({ id }, index) => {
-                      const ploIndex = form
-                        .getValues()
-                        .data[cloIndex].plos?.findIndex((plo) => plo == id);
-                      return (
-                        <Table.Td key={index}>
-                          <div className="flex items-start">
-                            <Checkbox
-                              size="sm"
-                              classNames={{
-                                input:
-                                  "bg-[black] bg-opacity-0 border-[1.5px] border-[#3E3E3E] cursor-pointer disabled:bg-gray-400",
-                                body: "mr-3 px-0",
-                                label:
-                                  "text-[14px] text-[#615F5F] cursor-pointer",
-                              }}
-                              checked={(
-                                form.getValues().data[cloIndex].plos as string[]
-                              ).includes(id)}
-                              onChange={(event) => {
-                                if (event.target.checked) {
-                                  form.insertListItem(
-                                    `data.${cloIndex}.plos`,
-                                    id
-                                  );
-                                } else if (ploIndex >= 0) {
-                                  form.removeListItem(
-                                    `data.${cloIndex}.plos`,
-                                    ploIndex
-                                  );
-                                }
-                              }}
-                            />
+                      <p className="">
+                        PLO-{no}{" "}
+                        <span className="text-red-500">
+                          {ploRequired.includes(id) && "required"}
+                        </span>
+                      </p>
+                      <p className="error-text mt-1">
+                        {validatePloRequired &&
+                          ploRequired.includes(id) &&
+                          !form
+                            .getValues()
+                            .data.some(({ plos }) =>
+                              (plos as string[]).includes(id)
+                            ) &&
+                          "Select CLO at least one"}
+                      </p>
+                    </Table.Th>
+                  ))}
+                </Table.Tr>
+              </Table.Thead>
+              <Table.Tbody>
+                {form.getValues().data.map(({ clo }, cloIndex) => {
+                  const cloItem = tqf3?.part2?.clo.find((e) => e.id == clo);
+                  return (
+                    <Table.Tr
+                      key={cloIndex}
+                      className="text-[13px] text-default"
+                    >
+                      <Table.Td
+                        key={form.key(`data.${cloIndex}.plos`)}
+                        style={{
+                          filter: "drop-shadow(2px 0px 2px rgba(0, 0, 0, 0.1))",
+                        }}
+                        className="!p-0 !py-1 sticky left-0 z-[1]"
+                        {...form.getInputProps(`data.${cloIndex}.plos`)}
+                      >
+                        <div className="flex gap-5 justify-start  items-center  px-[20px] py-2">
+                          <div className="text-secondary min-w-fit font-bold">
+                            CLO-{cloItem?.no}
                           </div>
-                        </Table.Td>
-                      );
-                    })}
-                  </Table.Tr>
-                );
-              })}
-            </Table.Tbody>
-          </Table>
+                          <p className="flex w-fit font-medium justify-between flex-col ">
+                            <span className="mb-2">{cloItem?.descTH}</span>
+                            <span>{cloItem?.descEN}</span>
+                            <span className="error-text mt-1">
+                              {
+                                form.getInputProps(`data.${cloIndex}.plos`)
+                                  .error
+                              }
+                            </span>
+                          </p>
+                        </div>
+                      </Table.Td>
+                      {coursePLO?.data?.map(({ id }, index) => {
+                        const ploIndex = form
+                          .getValues()
+                          .data[cloIndex].plos?.findIndex((plo) => plo == id);
+                        return (
+                          <Table.Td key={index}>
+                            <div className="flex items-start">
+                              <Checkbox
+                                size="sm"
+                                classNames={{
+                                  input:
+                                    "bg-[black] bg-opacity-0 border-[1.5px] border-[#3E3E3E] cursor-pointer disabled:bg-gray-400",
+                                  body: "mr-3 px-0",
+                                  label:
+                                    "text-[14px] text-[#615F5F] cursor-pointer",
+                                }}
+                                checked={(
+                                  form.getValues().data[cloIndex]
+                                    .plos as string[]
+                                ).includes(id)}
+                                onChange={(event) => {
+                                  if (event.target.checked) {
+                                    form.insertListItem(
+                                      `data.${cloIndex}.plos`,
+                                      id
+                                    );
+                                  } else if (ploIndex >= 0) {
+                                    form.removeListItem(
+                                      `data.${cloIndex}.plos`,
+                                      ploIndex
+                                    );
+                                  }
+                                }}
+                              />
+                            </div>
+                          </Table.Td>
+                        );
+                      })}
+                    </Table.Tr>
+                  );
+                })}
+              </Table.Tbody>
+            </Table>
+          </div>
         </div>
+      </>
+    ) : (
+      <div className="flex flex-col w-full h-full justify-center items-center">
+        {loading ? <Loading /> : "PLO Collection Not Found"}
       </div>
-    </>
+    )
   ) : (
     <div className="flex px-16  flex-row items-center justify-between h-full">
       <div className="flex justify-center  h-full items-start gap-2 flex-col">
