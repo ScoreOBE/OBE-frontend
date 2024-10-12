de215c7ca2549f27dfb81d702b9292dcadf7ae08
thanaporn.ch <thanaporn_chan@cmu.ac.th>
2024-10-13 05:31:09 +0700

    #validateL file upload

--- src/components/Modal/ModalUploadScore.tsx
+++ src/components/Modal/ModalUploadScore.tsx
@@ -1,6 +1,10 @@
 import { useState } from "react";
-import { Dropzone, IMAGE_MIME_TYPE } from "@mantine/dropzone";
-import { Group, Table, TextInput, rem } from "@mantine/core";
+import {
+  Dropzone,
+  FileRejection,
+  FileWithPath,
+  MS_EXCEL_MIME_TYPE,
+} from "@mantine/dropzone";
 import { Alert, Button, Modal } from "@mantine/core";
 import {
   IconBulb,
@@ -16,8 +20,10 @@ import {
 import { IModelCourse } from "@/models/ModelCourse";
 import gradescope from "@/assets/image/gradescope.png";
 import ModalStudentList from "./ModalStudentList";
-import { useSearchParams } from "react-router-dom";
 import ModalTemplateGuide from "./ModalTemplateGuide";
+import { showNotifications } from "@/helpers/functions/function";
+import { NOTI_TYPE } from "@/helpers/constants/enum";
+import * as XLSX from "xlsx";
 
 type Props = {
   opened: boolean;
@@ -26,9 +32,95 @@ type Props = {
 };
 
 export default function ModalUploadScore({ opened, onClose, data }: Props) {
-  const [params, setParams] = useSearchParams();
   const [openModalStudentList, setOpenModalStudentList] = useState(false);
   const [openModalTemplateGuide, setOpenModalTemplateGuide] = useState(false);
+  const [openModalUploadError, setOpenModalUploadError] = useState(false);
+  const [errorStudentId, setErrorStudentId] = useState<string[]>([]);
+  const [errorPoint, setErrorPoint] = useState<string[]>([]);
+
+  function isNumeric(value: any) {
+    return !isNaN(parseFloat(value)) && isFinite(value);
+  }
+
+  const getColumnAlphabet = (columnIndex: number) => {
+    let alphabet = "";
+    while (columnIndex >= 0) {
+      alphabet = String.fromCharCode((columnIndex % 26) + 65) + alphabet;
+      columnIndex = Math.floor(columnIndex / 26) - 1;
+    }
+    return alphabet;
+  };
+
+  const onUploadFile = async (files: FileWithPath[]) => {
+    const file = files[0];
+    if (file) {
+      const data = await file.arrayBuffer();
+      const workbook = XLSX.read(data);
+      for (const sheet of workbook.SheetNames) {
+        const worksheet = workbook.Sheets[sheet];
+        const resultsData: any[] = XLSX.utils.sheet_to_json(worksheet, {
+          // defval: "",
+          // header: 1,
+          // raw: true
+        });
+        const fullScore = resultsData.shift();
+        const description = resultsData.shift();
+
+        // console.log(fullScore);
+        // console.log(description);
+        // console.log(resultsData);
+
+        // Validate the studentId
+        const errorStudentIdList: string[] = [];
+        resultsData.forEach(({ studentId }, index) => {
+          if (
+            studentId &&
+            (!isNumeric(studentId) || studentId.toString().length !== 9)
+          ) {
+            const row = index + 4;
+            const column = getColumnAlphabet(1);
+            errorStudentIdList.push(`${column}${row}`);
+          }
+        });
+        // Validate the "point" field
+        const errorPointList: string[] = [];
+        resultsData.forEach((data, i) => {
+          Object.keys(data)
+            .slice(4)
+            .map((key, j) => {
+              if (data[key] && !isNumeric(data[key])) {
+                const row = i + 4;
+                const column = getColumnAlphabet(j + 5);
+                errorPointList.push(`${column}${row}`);
+              }
+            });
+        });
+        if (errorStudentIdList.length || errorPointList.length) {
+          files = [];
+          setErrorStudentId(errorStudentIdList);
+          setErrorPoint(errorPointList);
+          setOpenModalUploadError(true);
+        }
+      }
+    }
+  };
+
+  const onRejectFile = (files: FileRejection[]) => {
+    let title = "";
+    let message = "";
+    switch (files[0].errors[0].code) {
+      case "file-invalid-type":
+        title = "Invalid file type";
+        message = "File type must be .csv, xls or .xlsx";
+        break;
+      case "file-too-large":
+        title = "";
+        message = "";
+        break;
+    }
+    showNotifications(NOTI_TYPE.ERROR, title, message);
+  };
+
   // const studentData = [
   //   {
   //     filename: "Prelim 1",
@@ -132,6 +224,19 @@ export default function ModalUploadScore({ opened, onClose, data }: Props) {
         onClose={() => setOpenModalStudentList(false)}
         type="import"
       />
+      <Modal
+        opened={openModalUploadError}
+        onClose={() => setOpenModalUploadError(false)}
+        closeOnClickOutside={true}
+        title="Error upload score"
+        transitionProps={{ transition: "pop" }}
+        centered
+      >
+        <div>
+          <p>Student ID: {errorStudentId.join(", ")}</p>
+          <p>Point: {errorPoint.join(", ")}</p>
+        </div>
+      </Modal>
       {/* Main Modal */}
       <Modal.Root
         opened={opened}
@@ -296,64 +401,58 @@ export default function ModalUploadScore({ opened, onClose, data }: Props) {
               </Alert>
 
               <Dropzone
-                onDrop={(files) => console.log("accepted files", files)}
-                onReject={(files) => console.log("rejected files", files)}
+                onDrop={(files) => onUploadFile(files)}
+                onReject={(files) => onRejectFile(files)}
+                maxFiles={1}
                 maxSize={5 * 1024 ** 2}
-                accept={IMAGE_MIME_TYPE}
-                className=" hover:bg-gray-50 py-12 border-[#8f9ae37f] border-dashed bg-white cursor-pointer border-[2px] rounded-md"
+                accept={MS_EXCEL_MIME_TYPE}
+                className="bg-white hover:bg-gray-50 py-12 border-[#8f9ae37f] border-dashed cursor-pointer border-[2px] rounded-md"
               >
-                <Group
-                  justify="center"
-                  gap="xl"
-                  mih={220}
-                  style={{ pointerEvents: "none" }}
-                >
+                <div className="flex flex-col gap-3 justify-center items-center pointer-events-none">
                   <Dropzone.Accept>
                     <IconUpload
-                      style={{
-                        width: rem(52),
-                        height: rem(52),
-                        color: "var(--mantine-color-blue-6)",
-                      }}
+                      style={{ color: "var(--mantine-color-green-6)" }}
                       stroke={1.5}
+                      className="bg-green-100 size-16 p-3 rounded-full"
                     />
                   </Dropzone.Accept>
                   <Dropzone.Reject>
                     <IconX
-                      style={{
-                        width: rem(52),
-                        height: rem(52),
-                        color: "var(--mantine-color-red-6)",
-                      }}
+                      style={{ color: "var(--mantine-color-red-6)" }}
                       stroke={1.5}
+                      className="bg-red-200 size-16 p-3 rounded-full"
                     />
                   </Dropzone.Reject>
-
-                  <div className=" flex flex-col gap-3 justify-center items-center">
+                  <Dropzone.Idle>
                     <IconUpload
                       color="#5768d5"
                       stroke={1.5}
-                      className=" bg-[#DDE0FF] size-16 p-3 rounded-full"
+                      className="bg-[#DDE0FF] size-16 p-3 rounded-full"
                     />
-                    <p className="font-semibold text-default">
-                      <span className="text-secondary underline">
-                        {" "}
-                        Click to upload
-                      </span>{" "}
-                      or drag and drop
-                    </p>
-                    <p className="-mt-2 font-medium items-center justify-center text-center text-secondary text-b2">
-                      CSV or XLSX format only (up to 10MB)
-                    </p>
-                    <div className="flex flex-col font-medium  text-b2 text-red-500  items-center text-center justify-center">
-                      <div className="flex gap-2 items-center justify-center">
-                        <IconExclamationCircle color="red" className="size-4" />
-                        <p>Supports only Score OBE+</p>
-                      </div>
-                      <p> Gradescope assignment template</p>
+                  </Dropzone.Idle>
+
+                  {/* <IconUpload
+                      color="#5768d5"
+                      stroke={1.5}
+                      className=" bg-[#DDE0FF] size-16 p-3 rounded-full"
+                    /> */}
+                  <p className="font-semibold text-default">
+                    <span className="text-secondary underline">
+                      Click to upload
+                    </span>{" "}
+                    or drag and drop
+                  </p>
+                  <p className="-mt-2 font-medium items-center justify-center text-center text-secondary text-b2">
+                    CSV or XLSX format only (up to 10MB)
+                  </p>
+                  <div className="flex flex-col font-medium  text-b2 text-red-500  items-center text-center justify-center">
+                    <div className="flex gap-2 items-center justify-center">
+                      <IconExclamationCircle color="red" className="size-4" />
+                      <p>Supports only Score OBE+</p>
                     </div>
+                    <p> Gradescope assignment template</p>
                   </div>
-                </Group>
+                </div>
               </Dropzone>
               <div className=" rounded-md items-center border-[2px] border-[#8f9ae37f] py-4 gap-3 px-5 flex">
                 <div className="flex flex-col w-full">
@@ -367,7 +466,6 @@ export default function ModalUploadScore({ opened, onClose, data }: Props) {
                     </div>
                   </div>
                   <div className=" ml-9 mt-2 rounded-md h-2 bg-slate-300">
-                    {" "}
                     <div className="bg-secondary rounded-md w-72 h-2"></div>
                   </div>
                 </div>
