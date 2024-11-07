35e5a79cd708bce7e76b75a4f4d52777d15e093b
auswitch <90751246+auswitch@users.noreply.github.com>
2024-11-07 21:46:07 +0700

    add 100svh fix ipad screen

--- src/App.tsx
+++ src/App.tsx
@@ -87,7 +87,7 @@ function App() {
     <PageError />
   ) : (
     
-    <div className="flex h-screen w-screen text-default">
+    <div className="flex heig h-screen w-screen text-default">
       <AOSInit />
       {showSidebar && <Sidebar />}
       <div className="flex flex-col h-full w-full overflow-hidden">
