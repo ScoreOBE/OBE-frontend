739f2a1c630712191a5a923604f3313db4a71e4b
thanaporn.ch <thanaporn_chan@cmu.ac.th>
2024-11-26 16:19:26 +0700

    #fixed: Part1TQF5

--- src/models/ModelTQF5.ts
+++ src/models/ModelTQF5.ts
@@ -3,8 +3,40 @@ import { TQF_STATUS } from "@/helpers/constants/enum";
 export interface IModelTQF5 {
   id: string;
   status: TQF_STATUS;
-  part1?: any;
+  part1?: IModelTQF5Part1;
   part2?: any;
   part3?: any;
+  updatedAt: Date;
+}
+
+export interface IModelTQF5Part1 {
+  courseEval: {
+    sectionNo: number;
+    A: number;
+    Bplus: number;
+    B: number;
+    Cplus: number;
+    C: number;
+    Dplus: number;
+    D: number;
+    F: number;
+    W: number;
+    S: number;
+    U: number;
+    P: number;
+  }[];
+  gradingCriteria: {
+    A: string;
+    Bplus: string;
+    B: string;
+    Cplus: string;
+    C: string;
+    Dplus: string;
+    D: string;
+    F: string;
+    W: string;
+    S: string;
+    U: string;
+  };
   updatedAt: Date;
 }
