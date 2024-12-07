2cc63065d6062def6699f1f071cccbe028ee9551
auswitch <90751246+auswitch@users.noreply.github.com>
2024-12-07 17:09:13 +0700

    edit UI

--- src/pages/TQF/TQF5.tsx
+++ src/pages/TQF/TQF5.tsx
@@ -486,14 +486,14 @@ export default function TQF5() {
                 (tqf5Part == "part1" || tqf5.method) ? (
                   part.compo
                 ) : tqf3?.status != TQF_STATUS.DONE ? (
-                  <div className="flex px-16 sm:max-ipad11:px-8 flex-row items-center justify-between h-full">
+                  <div className="flex px-12 sm:max-ipad11:px-8 flex-row items-center justify-between h-full">
                     <div className="h-full  justify-center flex flex-col">
                       <p className="text-secondary text-[21px] font-semibold">
-                        TQF3 incompleted
+                        TQF 3 Completion Needed
                       </p>
                       <br />
                       <p className=" -mt-3 mb-6 text-b2 break-words font-medium leading-relaxed">
-                        Your TQF3 is incompleted in all part.
+                      It seems this TQF 3 for {courseNo} isn’t fully completed all parts yet. <br/> Review and fill in the missing sections to ensure everything is in place!
                       </p>
                     </div>
                     <img
