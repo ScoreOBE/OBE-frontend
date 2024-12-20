64283333c5c658cd5201c69e57104db04fa2ab31
thanaporn.ch <thanaporn_chan@cmu.ac.th>
2024-12-20 13:01:59 +0700

    #fixed: Loading, LoadingOverlay

--- src/components/Loading/LoadingOverlay.tsx
+++ src/components/Loading/LoadingOverlay.tsx
@@ -1,6 +1,6 @@
 export default function LoadingOverlay() {
   return (
-    <div className="h-[100vh] -mt-2 w-[100vw] fixed top-0 left-0 bg-black bg-opacity-20 z-[10000] flex justify-center items-center">
+    <div className="h-screen w-screen fixed top-0 left-0 bg-black bg-opacity-20 z-[10000] flex justify-center items-center">
       <l-newtons-cradle
         size="100"
         stroke-length="0.35"
