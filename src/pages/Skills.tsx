cd815c06ae7a0207151f68595e2d2347a4bcbb22
auswitch <90751246+auswitch@users.noreply.github.com>
2024-12-28 19:01:17 +0700

    add skills

--- src/pages/Skills.tsx
+++ src/pages/Skills.tsx
@@ -1,42 +1,42 @@
 import { useAppDispatch, useAppSelector } from "@/store";
 import { useEffect, useState } from "react";
-import { useNavigate, useParams, useSearchParams } from "react-router-dom";
-import { Alert, Button, Menu, Switch } from "@mantine/core";
-import Icon from "@/components/Icon";
-import IconExclamationCircle from "@/assets/icons/exclamationCircle.svg?react";
-import IconPencilMinus from "@/assets/icons/pencilMinus.svg?react";
-import IconStudent from "@/assets/icons/student.svg?react";
-import IconUpload from "@/assets/icons/upload.svg?react";
-import IconDots from "@/assets/icons/dots.svg?react";
-import IconUserGroup from "@/assets/icons/usersGroup.svg?react";
-import IconTrash from "@/assets/icons/trash.svg?react";
-import IconManageAdmin from "@/assets/icons/addCo.svg?react";
-import IconPlus2 from "@/assets/icons/plus2.svg?react";
-import IconExcel from "@/assets/icons/excel.svg?react";
-import { IModelCourse } from "@/models/ModelCourse";
-import { getOneCourse } from "@/services/course/course.service";
-import { editCourse, editSection, removeSection } from "@/store/course";
-import { getSectionNo, getUserName } from "@/helpers/functions/function";
-import { showNotifications } from "@/helpers/notifications/showNotifications";
-import PageError from "./PageError";
-import MainPopup from "@/components/Popup/MainPopup";
-import { COURSE_TYPE, NOTI_TYPE, ROLE } from "@/helpers/constants/enum";
-import ModalEditSection from "@/components/Modal/CourseManage/ModalEditSection";
-import Loading from "@/components/Loading/Loading";
-import { ROUTE_PATH } from "@/helpers/constants/route";
-import { IModelSection } from "@/models/ModelCourse";
-import {
-  deleteSection,
-  updateSectionActive,
-} from "@/services/section/section.service";
-import ModalAddSection from "@/components/Modal/CourseManage/ModalAddSection";
-import { IModelUser } from "@/models/ModelUser";
-import ModalManageIns from "@/components/Modal/CourseManage/ModalManageIns";
+import maintenace from "@/assets/image/maintenance.jpg";
 import { setShowNavbar, setShowSidebar } from "@/store/config";
-import ModalStudentList from "@/components/Modal/ModalStudentList";
-import ModalExportScore from "@/components/Modal/Score/ModalExportScore";
-import ModalUploadScore from "@/components/Modal/Score/ModalUploadScore";
+
 
 export default function Skills() {
-  return <>This is Skills Page</>;
+
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch(setShowSidebar(true));
+    dispatch(setShowNavbar(true));
+    
+  }, );
+
+
+  return <div className=" flex flex-col h-full w-full  overflow-hidden">
+  <div className="flex flex-row px-6 pt-3   items-center justify-between">
+    <div className="flex flex-col">
+      <p className="text-secondary text-[18px] font-semibold "></p>
+    </div>
+  </div>
+  <div className="flex items-center  !h-full !w-full justify-between px-[88px]">
+    <div className="h-full  translate-y-2  justify-center flex flex-col">
+      <p className="text-secondary text-[21px] font-semibold">
+        Skills is coming soon to{" "}
+        <span className="font-[600] text-transparent bg-clip-text bg-gradient-to-r from-[#4285f4] via-[#ec407a] via-[#a06ee1] to-[#fb8c00]">
+          ScoreOBE +{" "}
+        </span>{" "}
+      </p>
+      <br />
+      <p className=" -mt-4 mb-6 text-b2 break-words font-medium leading-relaxed">
+        Skills will be available for instructor February 2025.
+      </p>
+    </div>
+    <div className="h-full  w-[25vw] justify-center flex flex-col">
+      <img src={maintenace} alt="notFound"></img>
+    </div>
+  </div>
+</div>
 }
--- src/pages/Student/StdSkills.tsx
+++ src/pages/Student/StdSkills.tsx
@@ -1,7 +1,39 @@
+import { useAppDispatch, useAppSelector } from "@/store";
+import { setShowNavbar, setShowSidebar } from "@/store/config";
+import maintenace from "@/assets/image/maintenance.jpg";
+import { useEffect } from "react";
+
 export default function StdSkills() {
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch(setShowSidebar(true));
+    dispatch(setShowNavbar(true));
+  });
   return (
     <div className=" flex flex-col h-full w-full  overflow-hidden">
-      This is Student Skills
+    <div className="flex flex-row px-6 pt-3   items-center justify-between">
+      <div className="flex flex-col">
+        <p className="text-secondary text-[18px] font-semibold "></p>
+      </div>
     </div>
+    <div className="flex items-center  !h-full !w-full justify-between px-[88px]">
+      <div className="h-full  translate-y-2  justify-center flex flex-col">
+        <p className="text-secondary text-[21px] font-semibold">
+          Skills is coming soon to{" "}
+          <span className="font-[600] text-transparent bg-clip-text bg-gradient-to-r from-[#4285f4] via-[#ec407a] via-[#a06ee1] to-[#fb8c00]">
+            ScoreOBE +{" "}
+          </span>{" "}
+        </p>
+        <br />
+        <p className=" -mt-4 mb-6 text-b2 break-words font-medium leading-relaxed">
+          Skills will be available for students February 2025.
+        </p>
+      </div>
+      <div className="h-full  w-[25vw] justify-center flex flex-col">
+        <img src={maintenace} alt="notFound"></img>
+      </div>
+    </div>
+  </div>
   );
 }
