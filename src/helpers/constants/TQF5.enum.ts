863cf5b06ae84ce91a99cfe573a31753a1044a81
auswitch <90751246+auswitch@users.noreply.github.com>
2025-01-12 05:29:28 +0700

    fix multislider

--- src/helpers/constants/TQF5.enum.ts
+++ src/helpers/constants/TQF5.enum.ts
@@ -3,7 +3,7 @@ import { IModelTQF5 } from "@/models/ModelTQF5";
 export enum PartTopicTQF5 {
   part1 = "Part 1 - การประเมินกระบวนวิชา\nCourse Evaluation",
   part2 = "Part 2 - การเลือกหัวข้อประเมินตามวัตถุประสงค์ของกระบวนวิชา\nAssessment tool mapping to CLO",
-  part3 = "Part 3 - เกณฑ์การประเมินตามวัตถุประสงค์เฉพาะของกระบวนวิชา\nRubrics for CLO/CSO evaluation",
+  part3 = "Part 3 - เกณฑ์การประเมินตามวัตถุประสงค์เฉพาะของกระบวนวิชา\nRubrics for CLO evaluation",
 }
 
 export type ValidPartTopics = keyof Pick<
