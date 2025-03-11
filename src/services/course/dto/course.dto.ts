2bf2729d55da78db489071889141ddaa868e0195
thanaporn.ch <thanaporn_chan@cmu.ac.th>
2025-03-11 22:09:29 +0700

    #fixed: load more

--- src/services/course/dto/course.dto.ts
+++ src/services/course/dto/course.dto.ts
@@ -5,8 +5,8 @@ export class CourseRequestDTO {
   orderBy: string = "courseNo";
   orderType: string = "asc";
   search?: string;
-  page?: number;
-  limit?: number;
+  page?: number = 1;
+  limit?: number = 20;
   ignorePage?: boolean = false;
   curriculum?: string[] = [];
   tqf3?: string[] = [];
